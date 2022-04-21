81505bfe19e42e48dd2252305838b3d98712b7cd
lebedev-023046 <lebedev.023046@gmail.com>
2022-04-21 21:58:30 +0300

    feat: add pets-burger functionality

---
+++ shelter/pages/pets/js/index.js
@@ -0,0 +1,35 @@
+// =========  HAMBURGER  =========
+
+const hamburger = document.querySelector(".hamburger")
+const navigation = document.querySelector(".nav-list")
+const logo = document.querySelector(".logo")
+const headerContainerBurger = document.querySelector(".header-container")
+
+const body = document.querySelector("body")
+
+hamburger.addEventListener("click", () => {
+    navigation.classList.toggle("open")
+    hamburger.classList.toggle("open-icon")
+    logo.classList.toggle("logo-burger")
+    headerContainerBurger.classList.toggle("header-container-burger")
+    body.classList.toggle("body-scroll")
+})
+
+const closeBurger = (event) => {
+    const tagList = ['header', 'section', 'h2', 'p', 'article']
+    console.log(event.target.localName)
+    if (event.target.classList.contains("nav-link")) {
+        navigation.classList.remove("open")
+        hamburger.classList.remove("open-icon")
+        body.classList.toggle("body-scroll")
+        headerContainerBurger.classList.remove("header-container-burger")
+    }
+    else if (tagList.includes(event.target.localName)) {
+        navigation.classList.remove("open")
+        hamburger.classList.remove("open-icon")
+        headerContainerBurger.classList.remove("header-container-burger")
+        body.classList.remove("body-scroll")
+    }
+}
+
+addEventListener("click", closeBurger)
