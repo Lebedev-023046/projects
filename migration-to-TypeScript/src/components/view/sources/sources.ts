5c7c70092fb6105eb6be349aff219aafa0683b0f
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-02 20:55:04 +0300

    refactor: migrate sourse.js to sourst.ts

---
+++ migration-to-TypeScript/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import { ISourseItem } from '../../../types/index'
+
+class Sources {
+    draw(data: Array<ISourseItem>) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            const sourceClone = (sourceItemTemp as HTMLTemplateElement).content.cloneNode(true) as HTMLElement;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
+    }
+}
+
+export default Sources;
