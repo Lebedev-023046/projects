c264206d787902d3671a9e2ca88123081459ca48
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-03 15:55:15 +0300

    refactor: migrate appLoader.js to appLoader.ts

--- migration-to-TypeScript/src/components/controller/appLoader.ts
+++ migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '1ebd6f88d5f04980b776fd9e5812aaa5', // получите свой ключ https://newsapi.org/
+            apiKey: '1ebd6f88d5f04980b776fd9e5812aaa5',
         });
     }
 }
