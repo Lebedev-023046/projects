f13feaad98d9372876dfc03339a6ee9dbae284d2
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-03 18:41:52 +0300

    refactor: migrate app.js to app.ts

---
+++ migration-to-TypeScript/src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { IArticleCombiner } from '../../types/index'
+
+class App {
+    public controller: AppController
+    public view: AppView
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        (document
+            .querySelector('.sources') as HTMLElement)
+            .addEventListener('click', (e) => this.controller.getNews(e, (data:IArticleCombiner) => this.view.drawNews(data)));
+        this.controller.getSources((data: IArticleCombiner) => this.view.drawSources(data));
+    }
+}
+
+export default App;
