94cf8091655b195ab8ef37b67f3062f7ce736408
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-03 17:40:53 +0300

    refactor: migrate controller.js to controller.ts

--- migration-to-TypeScript/src/components/controller/controller.ts
+++ migration-to-TypeScript/src/components/controller/controller.ts
@@ -1,27 +1,28 @@
+import { Callback, Endpoint } from '../../types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources<T>(callback: Callback<T>): void {
         super.getResp(
             {
-                endpoint: 'sources',
+                endpoint: Endpoint.sources,
             },
             callback
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews<T>(e: PointerEvent, callback: Callback<T>): void {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                const sourceId = target.getAttribute('data-source-id')!;
+                if ((newsContainer.getAttribute('data-source') !== sourceId)) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
-                            endpoint: 'everything',
+                            endpoint: Endpoint.everything,
                             options: {
                                 sources: sourceId,
                             },
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
