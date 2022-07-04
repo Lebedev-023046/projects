72f1bc69c6c4d40b8b09ab96f6839cb376c04fe1
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-04 22:48:56 +0300

    refactor: change baseLink

--- migration-to-TypeScript/src/components/controller/appLoader.ts
+++ migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '1ebd6f88d5f04980b776fd9e5812aaa5',
         });
     }
