93ec42a2ca5f78988b12a94b048acc110fc0ff96
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-12 19:29:56 +0300

    feat: add filters block

---
+++ Online-Store/src/components/filteres/filteres.ts
@@ -0,0 +1,34 @@
+import wNumb from 'wnumb';
+import 'nouislider/dist/nouislider.css';
+import noUiSlider from 'nouislider';
+
+const slider_year = <HTMLElement>document.querySelector(".slider__year")
+const slider_quantity = <HTMLElement>document.querySelector(".slider__quantity")
+
+noUiSlider.create(slider_year, {
+    start: [2019, 2022],
+    connect: true,
+    range: {
+        'min': 2019,
+        'max': 2022
+    },
+    behaviour: 'tap-drag',
+    tooltips: true,
+    format: wNumb({
+        decimals: 0
+    })
+});
+
+noUiSlider.create(slider_quantity, {
+    start: [0, 40],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 40
+    },
+    behaviour: 'tap-drag',
+    tooltips: true,
+    format: wNumb({
+        decimals: 0
+    })
+});
--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -1,1 +1,3 @@
 import './styles/main.scss'
+import './components/filteres/filteres'
+
