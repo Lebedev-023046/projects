f0d19f15227a5ce3a727961fbbd795f316b3bcd7
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-15 23:44:30 +0300

    feat: add brandSortins and active buttons listeners

--- Online-Store/src/components/filteres/filteres.ts
+++ Online-Store/src/components/filteres/filteres.ts
@@ -1,10 +1,57 @@
 import wNumb from 'wnumb';
 import 'nouislider/dist/nouislider.css';
 import noUiSlider from 'nouislider';
+import { IJson } from '../../types/index'
+import { data, renderCards } from '../cards/cards'
 
 const slider_year = <HTMLElement>document.querySelector(".slider__year")
 const slider_quantity = <HTMLElement>document.querySelector(".slider__quantity")
 
+const brandBlock = <HTMLElement>document.querySelector('.purpose__choices-brand')
+const brandButtons = <NodeListOf<Element>>document.querySelectorAll('.brand__item')
+const sizeBlock = <HTMLElement>document.querySelector('.purpose__choices-size')
+const sizesBlock = <NodeListOf<Element>>document.querySelectorAll('.size__item')
+const colorBlock = <HTMLElement>document.querySelector('.purpose__choices-color')
+const popularBlock = <HTMLElement>document.querySelector('.purpose__choices-popular')
+
+
+brandBlock.addEventListener('click', (event: MouseEvent) => {
+    if (event.target instanceof HTMLButtonElement) {
+        if (event.target.classList.contains('brand__item')) {
+            event.target.classList.toggle('purpose-button__item_active')
+        }
+        filterByBrand(data)
+    }
+})
+
+sizeBlock.addEventListener('click', (event: MouseEvent) => {
+    if (event.target instanceof HTMLButtonElement) {
+        if (event.target.classList.contains('size__item')) {
+            sizesBlock.forEach(element => element.classList.remove('size__radio_active'))
+            event.target.classList.toggle('size__radio_active')
+        }
+    }
+})
+
+colorBlock.addEventListener('click', (event: MouseEvent) => {
+    if (event.target instanceof HTMLButtonElement) {
+        if (event.target.classList.contains('color__item')) {
+            event.target.classList.toggle('color__item_active')
+        }
+    }
+})
+
+popularBlock.addEventListener('click', (event: MouseEvent) => {
+    if (event.target instanceof HTMLButtonElement) {
+        if (event.target.classList.contains('popular-checkbox')) {
+            event.target.classList.toggle('popular-checkbox_active')
+        }
+    }
+})
+
+
+
+// noUiSlider functionality
 noUiSlider.create(slider_year, {
     start: [2019, 2022],
     connect: true,
@@ -33,11 +80,29 @@ noUiSlider.create(slider_quantity, {
     })
 });
 
-// search-bar functionality
-const cross = <HTMLDivElement>document.querySelector('.search__cancel')
-const input = <HTMLInputElement>document.querySelector('.search__input')
-cross.addEventListener('click', () => {
-    input.value = ''
-})
+
+const filterСriterias = (relevantButtons: NodeListOf<Element>) => {
+    const relevantValues: string[] = Array.from(brandButtons)
+                                            .filter((btn) => btn.classList.contains('purpose-button__item_active'))
+                                            .map((btn) => btn.innerHTML)
+    return relevantValues
+    }
+
+const filterByBrand = (data: Array<IJson>) => {
+    let res: Array<IJson> = []
+    let brandArr = filterСriterias(brandButtons)
+    if (brandArr.length>0) {
+        for (let i=0; i<brandArr.length;i++) {
+            let fData = data.filter(elem => elem.brand.toLowerCase().includes(brandArr[i].toLowerCase()))
+            fData.forEach(element => res.push(element))
+        }
+    renderCards(res)
+    }
+    else {
+        renderCards(data)
+    }
+}
+
+
 
 
