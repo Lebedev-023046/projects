fb1cf6b1aea670a11ddfb25969323b0f446584db
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-15 16:11:03 +0300

    feat: add json interface

---
+++ Online-Store/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface IJson {
+    id?: string,
+    name?: string,
+    size?: number[],
+    color?: string,
+    price?: number,
+    collection?: string,
+    img?: string,
+    year?: number,
+    popular?: string,
+    brand?: string,
+    quantity?: number
+}
