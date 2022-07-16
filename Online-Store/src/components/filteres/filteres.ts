60d207ee52229712bf80b51d2c5cd1f90eaaf724
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-16 11:57:30 +0300

    feat: add filters by Size, Color and Popularity

--- Online-Store/src/components/filteres/filteres.ts
+++ Online-Store/src/components/filteres/filteres.ts
@@ -8,11 +8,13 @@ const slider_year = <HTMLElement>document.querySelector(".slider__year")
 const slider_quantity = <HTMLElement>document.querySelector(".slider__quantity")
 
 const brandBlock = <HTMLElement>document.querySelector('.purpose__choices-brand')
-const brandButtons = <NodeListOf<Element>>document.querySelectorAll('.brand__item')
+const brandButtons = <NodeListOf<HTMLElement>>document.querySelectorAll('.brand__item')
 const sizeBlock = <HTMLElement>document.querySelector('.purpose__choices-size')
-const sizesBlock = <NodeListOf<Element>>document.querySelectorAll('.size__item')
+const sizeButtons = <NodeListOf<HTMLElement>>document.querySelectorAll('.size__item')
 const colorBlock = <HTMLElement>document.querySelector('.purpose__choices-color')
+const colorButtons = <NodeListOf<HTMLElement>>document.querySelectorAll('.color__item')
 const popularBlock = <HTMLElement>document.querySelector('.purpose__choices-popular')
+const popularButtons = <NodeListOf<HTMLElement>>document.querySelectorAll('.popular-checkbox')
 
 
 brandBlock.addEventListener('click', (event: MouseEvent) => {
@@ -27,9 +29,9 @@ brandBlock.addEventListener('click', (event: MouseEvent) => {
 sizeBlock.addEventListener('click', (event: MouseEvent) => {
     if (event.target instanceof HTMLButtonElement) {
         if (event.target.classList.contains('size__item')) {
-            sizesBlock.forEach(element => element.classList.remove('size__radio_active'))
             event.target.classList.toggle('size__radio_active')
         }
+        filterBySize(data)
     }
 })
 
@@ -38,6 +40,7 @@ colorBlock.addEventListener('click', (event: MouseEvent) => {
         if (event.target.classList.contains('color__item')) {
             event.target.classList.toggle('color__item_active')
         }
+        filterByColor(data)
     }
 })
 
@@ -46,6 +49,7 @@ popularBlock.addEventListener('click', (event: MouseEvent) => {
         if (event.target.classList.contains('popular-checkbox')) {
             event.target.classList.toggle('popular-checkbox_active')
         }
+        filterByPopular(data)
     }
 })
 
@@ -81,22 +85,79 @@ noUiSlider.create(slider_quantity, {
 });
 
 
-const filterСriterias = (relevantButtons: NodeListOf<Element>) => {
-    const relevantValues: string[] = Array.from(brandButtons)
-                                            .filter((btn) => btn.classList.contains('purpose-button__item_active'))
-                                            .map((btn) => btn.innerHTML)
+const filterСriterias = (relevantButtons: NodeListOf<HTMLElement>) => {
+    // console.log(Array.from(relevantButtons).forEach(element => console.log(element)))
+    const relevantValues: string[] = Array.from(relevantButtons)
+                                            .filter((btn) => btn.classList.contains('purpose-button__item_active') ||
+                                                             btn.classList.contains('size__radio_active') ||
+                                                             btn.classList.contains('popular-checkbox_active') ||
+                                                             btn.classList.contains('color__item_active'))
+                                            .map((btn) => btn.innerHTML || <string>btn.dataset.color)
     return relevantValues
-    }
+}
+
+// const filterCriteriasColor = (relevantButtons: NodeListOf<HTMLElement>) => {
+//     const relevantValues: string[] = Array.from(relevantButtons)
+//                                             .filter((btn) => btn.classList.contains('purpose-button__item_active') ||
+//                                                             btn.classList.contains('size__radio_active') ||
+//                                                             btn.classList.contains('popular-checkbox_active'))
+//                                             .map((btn) => btn.innerHTML)
+//     return relevantValues
+// }
 
 const filterByBrand = (data: Array<IJson>) => {
     let res: Array<IJson> = []
     let brandArr = filterСriterias(brandButtons)
-    if (brandArr.length>0) {
+    if (brandArr.length > 0) {
         for (let i=0; i<brandArr.length;i++) {
             let fData = data.filter(elem => elem.brand.toLowerCase().includes(brandArr[i].toLowerCase()))
             fData.forEach(element => res.push(element))
         }
-    renderCards(res)
+        renderCards(res)
+    }
+    else {
+        renderCards(data)
+    }
+}
+
+const filterBySize = (data: Array<IJson>) => {
+    let res: Array<IJson> = []
+    let sizeArr = filterСriterias(sizeButtons)
+    if (sizeArr.length > 0) {
+        for (let i=0; i<sizeArr.length;i++) {
+            let fData = data.filter(elem => elem.size.includes(Number(sizeArr[i])))
+            fData.forEach(element => res.push(element))
+        }
+        renderCards(res)
+    }
+    else {
+        renderCards(data)
+    }
+}
+
+const filterByColor = (data: Array<IJson>) => {
+    let res: Array<IJson> = []
+    let colorArr = filterСriterias(colorButtons)
+    console.log(colorArr)
+    if (colorArr.length > 0) {
+        for (let i=0; i<colorArr.length;i++) {
+            let fData = data.filter(elem => elem.color.includes((colorArr[i])))
+            fData.forEach(element => res.push(element))
+        }
+        renderCards(res)
+    }
+    else {
+        renderCards(data)
+    }
+}
+
+const filterByPopular = (data: Array<IJson>) => {
+    let res: Array<IJson> = []
+    let sizeArr = filterСriterias(popularButtons)
+    if (sizeArr.length > 0) {
+        let fData = data.filter(elem => elem.popular === 'yes')
+        fData.forEach(element => res.push(element))
+        renderCards(res)
     }
     else {
         renderCards(data)
