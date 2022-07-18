64d3a19f03e1357de40e0a5dee917251262dbf4f
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-18 22:50:16 +0300

    feat: add message

--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -1,3 +1,5 @@
 import './styles/main.scss'
 import './components/filteres/filteres'
 import './components/cards/cards'
+
+alert("Привет! Я не успел доделать localStorage, проверь, пожалуйста, работу в среду вечером. Спасибо за понимание")
