cc244e194634d577b3b0bd03d37f3ba11f0692de
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-19 16:46:36 +0300

    feat: add alert

--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -2,4 +2,5 @@ import './styles/main.scss'
 import './components/filteres/filteres'
 import './components/cards/cards'
 
-alert("Привет! Я не успел доделать localStorage, проверь, пожалуйста, работу в среду вечером. Спасибо за понимание")
+
+window.addEventListener('load', () => alert("Привет! У меня всего 20 карточек, поэтому поставил лимит на 15")) 
