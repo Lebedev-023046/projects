cc1e5516752fac5aacbda8d0bbc276fa01873879
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-22 22:40:32 +0300

    feat: add test-attempts

---
+++ Online-Store/src/test/filter.test.js
@@ -0,0 +1,5 @@
+//import { filterByBrand, filterBySize, filterByColor, filterByPopular, filterByYear, filterByQuantity, filterByInput, filterByValue } from '../components/filteres'
+
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
