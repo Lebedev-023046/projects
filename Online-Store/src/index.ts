d66d59447da9e9a5af78af44cb2b28f0aa716bb9
lebedev-023046 <lebedev.023046@gmail.com>
2022-07-22 22:41:07 +0300

    refactor: delete message

--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -1,6 +1,3 @@
 import './styles/main.scss'
 import './components/filteres/filteres'
 import './components/cards/cards'
-
-
-window.addEventListener('load', () => alert("Привет! У меня всего 20 карточек, поэтому поставил лимит на 16")) 
