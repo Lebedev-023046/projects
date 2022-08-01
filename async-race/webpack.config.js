a9d84ad842520ea2340a795f49e25be27109a59a
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-01 12:15:27 +0300

    init: srart async-race task

---
+++ async-race/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const autoprefixer = require('autoprefixer')
+
+const assetsPath = path.join(__dirname, './src', 'assets')
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", 'sass-loader', "postcss-loader"]
+            },
+
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        assetModuleFilename: 'assets/[hash][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         { from: path.join(__dirname, 'src/assets'), to: path.join(__dirname, 'dist/assets') }
+        //     ]
+        // })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'production';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
