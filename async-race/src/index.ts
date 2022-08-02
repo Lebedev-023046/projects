5e29ecb217b2912e5537b6f496df6e0f63412f2e
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-02 20:47:42 +0300

    refactor: make index.ts empty

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,33 +1,4 @@
 import './styles/main.scss'
-import { renderWinners } from  './components/winners/winners'
-import { renderMenu } from './components/garage/garage'
+import { render } from './components/renderUI/renderUI'
 
-const body = document.querySelector('body')
-const main = document.querySelector('body')
-
-const renderBaseHTMLStructure = (): void => {
-    if (body instanceof HTMLElement) {
-        body.innerHTML = `
-        <div class="wrapper">
-            <header class="header">
-                <button class="btn toGarage">To Garage</button>
-                <button class="btn toWinners">To Winners</button>
-            </header>
-            <main class="main"></main>
-            <footer class="footer">
-                <button class="btn prev">PREV</button>
-                <button class="btn next">NEXT</button>
-            </footer>
-        </div>
-        `
-    }
-}
-
-renderBaseHTMLStructure()
-
-
-const toWinners = <HTMLElement>document.querySelector('.toWinners')
-const toGarage = <HTMLElement>document.querySelector('.toGarage')
-
-toWinners?.addEventListener('click', renderWinners)
-toGarage?.addEventListener('click', renderMenu)
+render()
