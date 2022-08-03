5e2d9848b87c7e636ce393de04ee246aed64b0cf
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-04 00:40:33 +0300

    feat: add listeners for CRUD operations

--- async-race/src/components/listeners/listeners.ts
+++ async-race/src/components/listeners/listeners.ts
@@ -1,15 +1,60 @@
-import { newCarBody } from './listenerFunctions'
-import { createCar } from '../api/api'
+import { insertCarData, newCarBody, updateCarBody } from './listenerFunctions'
+import { createCar, deleteCar, getCar, updateCar } from '../api/api'
 import { renderGarage, updateState } from '../garage/garage'
+import { ICars } from '../../interfaces/cars'
+
+const body = document.querySelector('body')
 
 export const listeners = () => {
-    document.querySelector('.create-btn')?.addEventListener('click', async () => {
-        console.log('Click')
-        await createCar(newCarBody())
-        await updateState()
-        const garage = document.querySelector('.garage')
-        if (garage instanceof HTMLElement) {
-            garage.innerHTML = renderGarage()
-        }
-    })
+    if (body instanceof HTMLElement) {
+        body.addEventListener('click', async (event: MouseEvent) => {
+            if (event.target instanceof Element) {
+                if (event.target.classList.contains('create-btn')) {
+                    await createCar(newCarBody())
+                    await updateState()
+                    const garage = document.querySelector('.garage')
+                    if (garage instanceof HTMLElement) {
+                        garage.innerHTML = renderGarage()
+                    }   
+                }
+                if (event.target.classList.contains('update-btn')) {
+                    const updateBtn = document.querySelector('.update-btn')
+                    if (updateBtn instanceof HTMLElement) {
+                        await updateCar(updateCarBody(), +updateBtn.dataset.id!)
+                    }
+                    await updateState()
+                    const garage = document.querySelector('.garage')
+                    if (garage instanceof HTMLElement) {
+                        garage.innerHTML = renderGarage()
+                    }
+                }
+                if (event.target.classList.contains('select-btn')) {
+                    const updateBtn = document.querySelector('.update-btn')
+                    const updateClr = document.querySelector('.input-text-update')
+                    const updateName = document.querySelector('.input-color-update')
+                    
+                    const id: number = +event.target.id.split('select-car-')[1]
+                    const currentCar: ICars = await getCar(id)
+                    if (updateBtn instanceof HTMLElement && updateClr instanceof HTMLElement && updateName instanceof HTMLElement) {
+                        updateBtn.dataset.id = String(id)
+                        updateBtn.removeAttribute('disabled') 
+                        updateClr.removeAttribute('disabled') 
+                        updateName.removeAttribute('disabled') 
+                    }
+                    insertCarData(currentCar.name, currentCar.color)
+
+                    
+                }
+                if (event.target.classList.contains('delete-btn')) {
+                    const id: number = +event.target.id.split('delete-car-')[1]
+                    await deleteCar(id)
+                    await updateState()
+                    const garage = document.querySelector('.garage')
+                    if (garage instanceof HTMLElement) {
+                        garage.innerHTML = renderGarage()
+                    }
+                }
+            }
+        })
+    }
 }
