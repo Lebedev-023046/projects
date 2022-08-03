785e79c2a378dc4cd8baa9fc1315707e75d1e95a
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-03 16:59:10 +0300

    feat: add createCar function

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -1,8 +1,19 @@
+import { render } from "../renderUI/renderUI"
+
 export const getCars = async (page: number, limit=7) => {
     const response = await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
-
     return {
         items: await response.json(),
         count: response.headers.get('X-Total-Count')
     }
+}
+
+export const createCar = async (body: object) => {
+    return await fetch(`http://127.0.0.1:3000/garage`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
