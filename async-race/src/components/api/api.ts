d610b8c7d3912be5f898ff139be1462b9d003a3a
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-04 00:38:16 +0300

    feat: add updateCar and deleteCar funcs

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -1,11 +1,13 @@
-import { render } from "../renderUI/renderUI"
-
 export const getCars = async (page: number, limit=7) => {
     const response = await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
     return {
         items: await response.json(),
         count: response.headers.get('X-Total-Count')
     }
+}
+
+export const getCar = async (id: number) => {
+    return (await fetch(`http://127.0.0.1:3000/garage/${id}`)).json()
 }
 
 export const createCar = async (body: object) => {
@@ -15,5 +17,21 @@ export const createCar = async (body: object) => {
         headers: {
             'Content-Type': 'application/json'
         }
+    })
+}
+
+export const updateCar = async (body: object, id: number) => {
+    return await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+export const deleteCar = async (id: number) => {
+    return await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+        method: 'DELETE',
     })
 }
