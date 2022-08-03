49e757d857606eeee66e173d480cd2f2c30723aa
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-04 00:40:08 +0300

    feat: add updateCarBody and insertCarData funcs

--- async-race/src/components/listeners/listenerFunctions.ts
+++ async-race/src/components/listeners/listenerFunctions.ts
@@ -8,6 +8,21 @@ export const newCarBody = () => {
     return body
 }
 
+export const updateCarBody = () => {
+    const body: ICars = {
+        name: (document.querySelector('.input-text-update') as HTMLInputElement).value,
+        color: (document.querySelector('.input-color-update') as HTMLInputElement).value
+    }
+    return body
+}
+
+export const insertCarData = (name: string, color: string) => {
+    (document.querySelector('.input-color-update') as HTMLInputElement).value = color,
+    (document.querySelector('.input-text-update') as HTMLInputElement).value = name
+}
+
+
+
 
 
 
