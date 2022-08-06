ed6852ccb3f7d7df469536420aea1507a609ddb7
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-06 12:37:18 +0300

    feat: add getWInners func

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -1,3 +1,5 @@
+import { IWinners } from '../../interfaces/winners'
+
 export const getCars = async (page: number, limit=7) => {
     const response = await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
     return {
@@ -34,4 +36,16 @@ export const deleteCar = async (id: number) => {
     return await fetch(`http://127.0.0.1:3000/garage/${id}`, {
         method: 'DELETE',
     })
+}
+
+export const getWinners = async (page: number, limit=10) => {
+    const response = await fetch(`http://127.0.0.1:3000/winners?_page=${page}&_limit=${limit}`)
+    const items = await response.json()
+
+    return {
+        items: await Promise.all(items.map(async (winner: IWinners) => ({
+            ...winner, car: await getCar(winner.id)
+        }))),
+        count: response.headers.get('X-Total-Count')
+    }
 }
