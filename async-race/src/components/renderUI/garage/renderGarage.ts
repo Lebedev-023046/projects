7473abaa4ec9a07011fdd6f2e716d7c7dfa96d64
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-06 12:41:21 +0300

    refactor: add convenient file sructure

---
+++ async-race/src/components/renderUI/garage/renderGarage.ts
@@ -0,0 +1,43 @@
+import { ICars } from '../../../interfaces/cars'
+import { renderCar } from './renderCar'
+import globalState from '../../globalState/globalState'
+import { getCars } from '../../api/api'
+
+export const updateState = async () => {
+    const { items, count } = await getCars(1)
+    globalState.cars = items
+    globalState.carsCount = count
+}
+
+export const renderMenu = () =>  `
+    <div class="menu">
+        <div>
+            <input required class="input-text-create" type="text" placeholder="name"/>
+            <input class="input-color-create" type="color"/>
+            <button class="btn create-btn">create</buttton>
+        </div>
+        <div>
+            <input disabled class="input-text-update" type="text" placeholder="name">
+            <input disabled class="input-color-update" type="color">
+            <input disabled class="btn update-btn" data-id="" type="submit" value="update">
+        </div>
+        <div>
+            <button class="btn race-btn">race</button>
+            <button class="btn reset-btn">reset</button>
+            <button class="btn generate-btn">generate cars</button>
+        </div>
+    </div>`
+
+export const renderGarage = () => {
+    return `${renderMenu()}
+    <div class="cars-view">
+        <h1 class="garage-items">Garage #${globalState.carsCount}</h1>
+        <h2 class="page-number">Page  #${1}</h2>
+        <ul class="garage-list">
+            ${globalState.cars.map(((elem: ICars) => `
+                <li>${renderCar(elem)}</li>
+            `))}
+        </ul>
+    </div>
+    `
+} 
---
+++ async-race/src/components/renderUI/winners/renderWinners.ts
@@ -0,0 +1,47 @@
+import { IWinners } from '../../../interfaces/winners'
+import { getWinners } from '../../api/api'
+import globalState from '../../globalState/globalState'
+import { renderCarImg } from '../garage/renderCar'
+
+const body = document.querySelector('body')
+
+const winners = (): string[]  => {
+    let winnersArr: string[] = []
+    globalState.winners.forEach((elem, index) => {
+        winnersArr.push(`
+        <tr>
+            <td>${index + 1}</td>
+            <td>${renderCarImg(elem.car.color)}</td>
+            <td>${elem.car.name}</td>
+            <td>${elem.wins}</td>
+            <td>${elem.time}</td>
+        </tr>
+        `)
+    })
+    return [...winnersArr]
+}
+
+export let renderWinners = ()  =>   `
+    <div class "wrapper">
+        <h1 class "winners-number">Winners #${globalState.winnersCount}</h1>
+        <h2 class "pages-number">Page  #${globalState.winnersPage}</h2>
+        <table class="winners-table">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>Car</th>
+                    <th>Name</th>
+                    <th>Wins</th>
+                    <th>Best time (seconds)</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${winners()}
+            </tbody>
+        </table>
+    </div>
+    `
+
+// console.log(await getWinners(1))
+
+
