d50fa8be0be7432d66307ba11bf3b8ba7c61af28
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-09 21:37:13 +0300

    feat: add engine operations and add return-types

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -1,72 +1,98 @@
-import { Response } from '../../interfaces/params'
-import { IWinners } from '../../interfaces/winners'
+import { ICars } from '../../interfaces/cars';
+import { Isuccess } from '../../interfaces/drive';
+import { IParams } from '../../interfaces/params';
+import { IWinners } from '../../interfaces/winners';
 
-export const getCars = async (page: number, limit=7) => {
-    const response = await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
+export const getCars = async (page: number, limit = 7): Promise<{ items: ICars[]; count: string | null }> => {
+    const response = await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`);
     return {
         items: await response.json(),
-        count: response.headers.get('X-Total-Count')
-    }
-}
+        count: response.headers.get('X-Total-Count'),
+    };
+};
 
-export const getCar = async (id: number) => {
-    return (await fetch(`http://127.0.0.1:3000/garage/${id}`)).json()
-}
+export const getCar = async (id: number): Promise<ICars> => {
+    return (await fetch(`http://127.0.0.1:3000/garage/${id}`)).json();
+};
 
-export const createCar = async (body: object) => {
-    return await fetch(`http://127.0.0.1:3000/garage`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-}
+export const createCar = async (body: object): Promise<ICars> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/garage`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    ).json();
+};
 
-export const updateCar = async (body: object, id: number) => {
-    return await fetch(`http://127.0.0.1:3000/garage/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-}
+export const updateCar = async (body: object, id: number): Promise<ICars> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    ).json();
+};
 
-export const deleteCar = async (id: number) => {
-    return await fetch(`http://127.0.0.1:3000/garage/${id}`, {
-        method: 'DELETE',
-    })
-}
+export const deleteCar = async (id: number): Promise<ICars> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+            method: 'DELETE',
+        })
+    ).json();
+};
 
-export const getWinners = async (page: number, limit=10) => {
-    const response = await fetch(`http://127.0.0.1:3000/winners?_page=${page}&_limit=${limit}`)
-    const items = await response.json()
+export const getWinners = async (page: number, limit = 10): Promise<{ items: ICars[]; count: string | null }> => {
+    const response = await fetch(`http://127.0.0.1:3000/winners?_page=${page}&_limit=${limit}`);
+    const items = await response.json();
 
     return {
-        items: await Promise.all(items.map(async (winner: IWinners) => ({
-            ...winner, car: await getCar(winner.id)
-        }))),
-        count: response.headers.get('X-Total-Count')
-    }
-}
+        items: await Promise.all(
+            items.map(async (winner: IWinners) => ({
+                ...winner,
+                car: await getCar(winner.id),
+            }))
+        ),
+        count: response.headers.get('X-Total-Count'),
+    };
+};
 
-export const createWinner = async (body: object) => {
-    return await fetch(`http://127.0.0.1:3000/winner`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-}
+export const createWinner = async (body: object): Promise<ICars> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/winner`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    ).json();
+};
 
-export const startEngine = async (id: number) => {
-    return (await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=started`, {
-        method: 'PATCH'
-    })).json()
-}
+export const startEngine = async (id: number): Promise<IParams> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=started`, {
+            method: 'PATCH',
+        })
+    ).json();
+};
 
-export const stopEngine = async (id: number) => {
-    return (await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=stopped`)).json()
-}
+export const stopEngine = async (id: number): Promise<IParams> => {
+    return (
+        await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=stopped`, {
+            method: 'PATCH',
+        })
+    ).json();
+};
+
+export const drive = async (id: number): Promise<Isuccess> => {
+    const result = await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=drive`, {
+        method: 'PATCH',
+    });
+    return result.status === 200 ? { ...(await result.json()) } : { success: false };
+};
