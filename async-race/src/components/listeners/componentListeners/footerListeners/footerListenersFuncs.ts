38657a41280c1852882c62c6b7822d7b7c8b4469
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-11 15:23:50 +0300

    feat: add pagination on both pages

--- async-race/src/components/listeners/componentListeners/footerListeners/footerListenersFuncs.ts
+++ async-race/src/components/listeners/componentListeners/footerListeners/footerListenersFuncs.ts
@@ -1,25 +1,45 @@
 import globalState from "../../../globalState/globalState"
 import { renderGarage } from "../../../renderUI/garage/renderGarage"
-import { updateGarageState } from "../../utils"
+import { renderWinners } from "../../../renderUI/winners/renderWinners"
+import { updateGarageState, updateWinnerState } from "../../utils"
 
 export const prevPressBtn = async (): Promise<void> => {
     const garage = document.querySelector('.garage')
-    if (garage instanceof HTMLElement) {
-        if (globalState.garagePage > 1) {
-            globalState.garagePage -= 1
-            await updateGarageState()
-            garage.innerHTML = renderGarage()
+    const winners = document.querySelector('.winners')
+    if (garage instanceof HTMLElement && winners instanceof HTMLElement) {
+        if (garage.style.display === 'block') { 
+            if (globalState.garagePage > 1) {
+                globalState.garagePage -= 1
+                await updateGarageState()
+                garage.innerHTML = renderGarage()
+            }
+        }else {
+            if (globalState.winnersPage > 1) {
+                globalState.winnersPage -= 1
+                await updateWinnerState()
+                winners.innerHTML = renderWinners()
+            }
         }
     }
 }
 export const nextPressBtn = async (): Promise<void> => {
     const garage = document.querySelector('.garage')
-    if (garage instanceof HTMLElement) {
-        if (globalState.garagePage * 7 < Number(globalState.carsCount)) {
-            globalState.garagePage += 1
-            await updateGarageState()
-            garage.innerHTML = renderGarage()
+    const winners = document.querySelector('.winners')
+    if (garage instanceof HTMLElement && winners instanceof HTMLElement) {
+        if (garage.style.display === 'block') {
+            if (globalState.garagePage * 7 < Number(globalState.carsCount)) {
+                globalState.garagePage += 1
+                await updateGarageState()
+                garage.innerHTML = renderGarage()
+            }
+        }else {
+            if (globalState.winnersPage * 7 < Number(globalState.winnersCount)) {
+                globalState.winnersPage += 1
+                await updateWinnerState()
+                winners.innerHTML = renderWinners()
+            }
         }
+
     }
 }
 
