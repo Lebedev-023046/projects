3f816c43c082d58690ee9900c6972fa6f6414497
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-11 15:24:25 +0300

    feat: add createWinnre functionality

--- async-race/src/components/listeners/componentListeners/raceLIsteners/raceLIsteners.ts
+++ async-race/src/components/listeners/componentListeners/raceLIsteners/raceLIsteners.ts
@@ -1,5 +1,8 @@
+import { createWinner, getCar } from "../../../api/api"
 import globalState from "../../../globalState/globalState"
-import { getIdxes } from "../../utils"
+import { renderWinnerTime } from "../../../renderUI/garage/renderGarage"
+import { getIdxes, updateWinnerState } from "../../utils"
+import { renderSortedView } from "../winnerListeners/winnerListenersFuncs"
 import { startRaceCars, stopRaceCars } from "./raceLIstenersFuncs"
 
 const body = document.querySelector('body')
@@ -9,10 +12,19 @@ export const raceListeners = (): void => {
         body.addEventListener('click', async (event: MouseEvent) => {
             if (event.target instanceof Element) {
                 if (event.target.classList.contains('race-btn')) {
+                    stopRaceCars(await getIdxes())
                     const idxes = await getIdxes()
+                    let results = []
                     for (let i=0;i<idxes.length;i++) {
-                        startRaceCars(idxes[i])
+                        let promise = startRaceCars(idxes[i])
+                        results.push(promise)
                     }
+                    const resultsCarsArr = await Promise.all(results)
+                    const winner = resultsCarsArr.filter(car => car.success).sort((a,b) => a.time - b.time)[0]
+                    renderWinnerTime((await getCar(winner.id)).name, winner.time)
+                    await createWinner({id: winner.id, wins: 1, time: winner.time / 1000})
+                    await updateWinnerState()
+                    renderSortedView('time')
                 }
                 if (event.target.classList.contains('reset-btn')) {
                     const idxes = await getIdxes()
